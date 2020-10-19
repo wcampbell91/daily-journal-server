2eb721c77752bbcceec39552ca314eb47839d4fa
Billy Campbell <wcampbell91@gmail.com>
2020-10-19 18:42:18 -0500

    added search by keyword in entry

---
+++ Moods/__init__.py
@@ -0,0 +1,1 @@
+from .request import get_all_moods, get_single_mood, delete_mood
---
+++ daily-journal-server.session.sql
@@ -0,0 +1,8 @@
+INSERT INTO Journal_entries (id, Concept, Entry, mood_id, date)
+VALUES (
+    id:INTEGER,
+    'Concept:TEXT',
+    'Entry:TEXT',
+    mood_id:INTEGER,
+    date:INTEGER
+  );
